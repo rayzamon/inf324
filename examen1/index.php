02383a7523e52024f8481ecde5c3d652f3e23833
rayzamon <146770976+rayzamon@users.noreply.github.com>
2023-10-07 18:10:42 -0400

    Add files via upload

---
+++ examen1/index.php
@@ -0,0 +1,40 @@
+<?php
+include "cabecera.inc.php"
+?>
+<nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <img src="images/logocarrera.png" alt="">
+            <span>
+              INFORMACIÓN
+            </span>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="">CASA <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#infocarrera">CARRERA </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#infokardex">KARDEX </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#infoinsti">INSTITUTO DE INVESTIGACIONES</a>
+                </li>
+              </ul>
+            </div>
+            
+          </div>
+        </nav>
+      </div>
+
+<?php
+include "pie.inc.php"
+?>
---
+++ examen1/pie.inc.php
@@ -0,0 +1,232 @@
+</header>
+    <!-- end header section -->
+    <!-- slider section -->
+    <section class=" slider_section position-relative">
+
+      <div class="slider_bg-container">
+
+      </div>
+      <div class="slider-container">
+
+        <div class="detail-box">
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="sr-only">Next</span>
+          </a>
+          <h1>
+            INFORMACIÓN <br>
+            CARRERA DE INFORMATICA, <br>
+            KARDEX E <br>
+            INSTITUTO DE INVESTIGACIÓN
+          </h1>
+          <p>
+            En la siguiente pagina se presenrará la información
+            necesaria sobre la carrera de informática, el kardex de ma misma carreera 
+            y el instituto de investigaciones
+         </p>
+          <div>
+            <a href="" class="slider-link">
+              CONTACT US
+            </a>
+          </div>
+        </div>
+        <div class="img-box">
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="images/carrera1.jpg" alt="">
+              </div>
+              <div class="carousel-item">
+                <img src="images/carrera2.jpg" alt="">
+              </div>
+              <div class="carousel-item">
+                <img src="images/carrera3.jpg" alt="">
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+    <!-- end slider section -->
+  </div>
+
+  <!-- acerca de la carrera -->
+  <section class="about_section layout_padding">
+    <div class="container">
+      <div class="row" id="infocarrera">
+        <div class="col-md-6">
+          <div class="detail-box" >
+            <h6>
+              sobre la
+            </h6>
+            <div class="custom_heading-container">
+              <h2>
+                carrera de informática
+              </h2>
+              <hr>
+            </div>
+            <p>
+                La incorporación de las Tecnologías de Información y Comunicación (TIC) en diferentes ámbitos; 
+                impulsan a realizar cambios en el sistema de educación superior para tener nuevos medios y métodos de aprendizaje, 
+                cursos en línea didácticos y pedagógicos, control de los procesos burocráticos, la intercomunicación entre sus componentes a distancia; 
+                que con el transcurrir del tiempo las TIC serán un medio de comunicación habitual a la que se debe habituar.
+            <div>
+              <a href="http://www.informatica.edu.bo/">
+                Más información
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="img-box">
+            <img src="images/logocarrera.png" alt="">
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- kardex -->
+  <section class="service_section layout_padding">
+    <div class="container">
+      <div class="d-flex flex-column align-items-end" id="infokardex">
+        <div class="custom_heading-container">
+          <hr>
+          <h2>
+            Kardex academico de informática
+          </h2>
+
+        </div>
+        <p>
+            El Kardex Académico se encuentra en el 2do patio del Monoblock, edif. de Informática, 
+            Piso 2, cerca a la entrada de la calle Federico Zuazo.
+        </p>
+      </div>
+    </div>
+        <div class="layout_padding-top layout_padding2-bottom">
+            <div class="row" >
+              
+              <div class="col-md-7">
+                <div class="map-box">
+                  <div id="map">
+                    <div class="map-responsive">
+                      <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2274.618292150087!2d-68.13027117583569!3d-16.504034815012442!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7efa3457461b1e6a!2sCarrera+de+Inform%C3%A1tica+-+UMSA!5e0!3m2!1ses-419!2sbo!4v1550576981909"
+                        width="600" height="300" frameborder="0" style="border: 5%;; width: 100%;" allowfullscreen></iframe>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-5">
+                <div class="detail-box" >
+                    
+                    <p>
+                        ATENCIÓN Y CONSULTAS <br>
+                        Telf. WhatsApp: 67043390 - 76241931 <br>
+                        HORARIO DE ATENCIÓN: <br>
+                        HRS. 8:00 a 14:00 (LUNES A VIERNES) <br>
+                    </p>
+                    
+                  </div>
+                </div>
+
+                
+                
+
+
+
+              </div>
+            </div>
+          </div>
+    
+    <div class="read-btn">
+      <a href="http://www.informatica.edu.bo/kardex/">
+        Más Información
+      </a>
+    </div>
+  </section>
+
+
+  <!-- acerca del instituto -->
+  <section class="about_section layout_padding">
+    <div class="container">
+      <div class="row" id="infoinsti">
+        <div class="col-md-6">
+          <div class="detail-box" >
+            <h6>
+              sobre el
+            </h6>
+            <div class="custom_heading-container">
+              <h2>
+                Instituto de investigaciones
+              </h2>
+              <hr>
+            </div>
+            <p>
+                En el Instituto de Investigaciones de Informática los Docentes también realizan actividades
+                 de investigación enmarcadas en las líneas de investigación, las exigencias de la sociedad, 
+                 la evolución de la ciencia y tecnología informática. Así como la interacción social para la difusión
+                y capacitación de la informática en la sociedad, que permita la inclusión social y reduzca 
+                la brecha digital.
+
+
+            <div>
+              <a href="http://instituto.informatica.edu.bo/">
+                Más información
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="img-box">
+            <img src="images/logoinstituto.png" alt="">
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- info section -->
+  <section class="info_section layout_padding">
+    <div class="container">
+      <div class="info_social" id="infoinsti">
+        <div>
+          <a href="https://www.facebook.com/CarreraDeInformaticaUmsa">
+            <img src="images/fb.png" alt="">
+          </a>
+        </div>
+        <div>
+            <a href="https://twitter.com/UmsaInformatica">
+              <img src="images/twitter.png" alt="">
+          </a>
+        </div>
+        <div>
+          <a href="https://www.youtube.com/channel/UCvByx4xqk8iX8oy5jQeJdLA">
+            <img src="images/youtube.png" alt="">
+          </a>
+        </div>
+        
+      </div>
+    </div>
+    <div>
+        <p>
+          Trabajo realizado por Univ. Rayza Andrea Montaño Mariaca
+        </p>
+      </div>
+  </section>
+
+
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+
+</body>
+</body>
+
+</html>
